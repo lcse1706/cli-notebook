601d5545554949049e086499dcbc82552ac73a9c
lukczar1706 <lcse.2001@gmail.com>
2023-08-11 12:40:57 +0200

    pull up project from react-typescript-projects repo

---
+++ packages/local-client/src/bundler/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,31 @@
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        // Handle root entry file of 'index.js'
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+        // Handle relative path in a module
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href,
+          };
+        }
+
+        // Handle main file of a module
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
